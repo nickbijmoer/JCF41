4463e38731974653b5a46843b5ac5bce06bb45e3
BartMemelink <b.memelink@student.fontys.nl>
2016-02-24 13:39:07 +0100

    opdracht 2

--- WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
+++ WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
@@ -5,12 +5,11 @@ package woordenapplicatie.gui;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -29,27 +28,28 @@ import javafx.scene.control.TextArea;
  * @author frankcoenen
  */
 public class WoordenController implements Initializable {
-   private List<String> words;
-   private static final String DEFAULT_TEXT =   "Een, twee, drie, vier\n" +
-                                                "Hoedje van, hoedje van\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van papier\n" +
-                                                "\n" +
-                                                "Heb je dan geen hoedje meer\n" +
-                                                "Maak er één van bordpapier\n" +
-                                                "Eén, twee, drie, vier\n" +
-                                                "Hoedje van papier\n" +
-                                                "\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van, hoedje van\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van papier\n" +
-                                                "\n" +
-                                                "En als het hoedje dan niet past\n" +
-                                                "Zetten we 't in de glazenkas\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van papier";
-    
+
+    private List<String> words;
+    private static final String DEFAULT_TEXT = "Een, twee, drie, vier\n"
+            + "Hoedje van, hoedje van\n"
+            + "Een, twee, drie, vier\n"
+            + "Hoedje van papier\n"
+            + "\n"
+            + "Heb je dan geen hoedje meer\n"
+            + "Maak er één van bordpapier\n"
+            + "Eén, twee, drie, vier\n"
+            + "Hoedje van papier\n"
+            + "\n"
+            + "Een, twee, drie, vier\n"
+            + "Hoedje van, hoedje van\n"
+            + "Een, twee, drie, vier\n"
+            + "Hoedje van papier\n"
+            + "\n"
+            + "En als het hoedje dan niet past\n"
+            + "Zetten we 't in de glazenkas\n"
+            + "Een, twee, drie, vier\n"
+            + "Hoedje van papier";
+
     @FXML
     private Button btAantal;
     @FXML
@@ -67,7 +67,7 @@ public class WoordenController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         taInput.setText(DEFAULT_TEXT);
     }
-    
+
     @FXML
     private void aantalAction(ActionEvent event) {
         String temp = DEFAULT_TEXT;
@@ -75,12 +75,12 @@ public class WoordenController implements Initializable {
         temp = temp.replaceAll(",", " ");
         temp = temp.replaceAll("  ", " ");
 
-        words = Arrays.asList(temp.split(" "));        
+        words = Arrays.asList(temp.split(" "));
         System.out.println(words.size());
-        
+
         Set<String> UniqueWords = new HashSet();
         for (String word : words) {
-        UniqueWords.add(word);        
+            UniqueWords.add(word);
         }
         System.out.println(UniqueWords.size());
 
@@ -91,45 +91,61 @@ public class WoordenController implements Initializable {
         String temp = DEFAULT_TEXT;
         temp = temp.replaceAll("(\r\n|\n)", " ");
         temp = temp.replaceAll(",", " ");
-         List words = Arrays.asList(temp.split("\\ "));
-         System.out.println(words);
-//         List<String> items = Arrays.asList(str.split("\\s*,\\s*"));
+        temp = temp.replaceAll("\'t", " ");
+        temp = temp.replaceAll("  ", " ");
+        temp = temp.replaceAll("   ", " ");
+
+        words = Arrays.asList(temp.split(" "));
+
+        Collections.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareToIgnoreCase(s2);
+            }
+        });
+        Collections.reverse(words);
+        
+        String temp2 = words.toString();
+        temp2 = temp2.replaceAll(",", "\n");
+        temp2 = temp2.replace("[", "");
+        temp2 = temp2.replace("]", "");
+        
+        taInput.setText(temp2);
     }
 
     @FXML
     private void frequentieAction(ActionEvent event) {
-    TreeMap <String, Integer> myMap  = new TreeMap <String, Integer> ();
-    
-     String temp = DEFAULT_TEXT;
+        TreeMap<String, Integer> myMap = new TreeMap<String, Integer>();
+
+        String temp = DEFAULT_TEXT;
         temp = temp.replaceAll("(\r\n|\n)", " ");
         temp = temp.replaceAll(",", " ");
         temp = temp.replaceAll("  ", " ");
 
-        String[] input = temp.split(" ");     
-        
-    for(int f = 0; f < input.length; f++) {         
-        String key = input[f].toUpperCase();
-        if(input[f].length() > 1) {
-            if(myMap.get(key) == null) {
-                myMap.put(key, 1);
-                }   
-            else {
+        String[] input = temp.split(" ");
+
+        for (int f = 0; f < input.length; f++) {
+            String key = input[f].toUpperCase();
+            if (input[f].length() > 1) {
+                if (myMap.get(key) == null) {
+                    myMap.put(key, 1);
+                } else {
                     int value = myMap.get(key).intValue();
                     value++;
                     myMap.put(key, value);
+                }
             }
-        }       
-    }       
-    
-    for(Map.Entry<String, Integer> entry : myMap.entrySet()) {
-        System.out.println(entry.getKey() + " : "+ entry.getValue());
-    }
+        }
+
+        for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
 
     }
 
     @FXML
     private void concordatieAction(ActionEvent event) {
-         throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-   
+
 }
